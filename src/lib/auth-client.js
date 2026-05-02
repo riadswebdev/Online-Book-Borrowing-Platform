a8692c1df4e6e2aa43e7b0aeab022bc378d523cc
djriad157764-creator <djriad157764@gmail.com>
2026-05-02 23:11:14 +0600

    Fix: issue

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "https://online-book-borrowing-platform-zeta.vercel.app/",
+  baseURL: "https://online-book-borrowing-platform-zeta.vercel.app",
 });
 export const { signIn, signUp, useSession } = createAuthClient();
